74b4df12e92bc677fe89064dff5faef13c248ce1
Khim22 <khimyang22@gmail.com>
2019-05-14 21:52:57 +0800

    init

---
+++ handler.js
@@ -0,0 +1,19 @@
+import { graphqlLambda, graphiqlLambda } from 'apollo-server-lambda'
+import { makeExecutableSchema} from 'graphql-tools'
+import { schema } from './schema'
+import { resolvers } from './resolvers'
+
+const pokeGraphQLSchema = makeExecutableSchema({
+    typeDefs: schema,
+    resolvers
+});
+
+exports.graphqlHandler = function graphqlHandler(event, context, callback){
+    function callbackWithHeaders(error, output){
+        output.headers['Access-Control-Allow-Origin'] = '*';
+        callback(error, output)
+    }
+
+    const handler = graphqlLambda({schema: pokeGraphQLSchema});
+    return handler(event, context, callbackWithHeaders)
+}
